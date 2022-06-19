b2020c9ace7a9cdeaa645d37a1dd831ec5af922d
ali.rabiee <a.rabiee@fidilio.com>
2022-06-19 18:06:29 +0430

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,101 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
 
-function App() {
+function App () {
+
+  const [form, setForm] = useState({
+    username: "",
+    email: "",
+    comment: "",
+    country: "uk", //=> if you want default value
+    gender: "other", //=> if you want default value
+    agree: false,
+  });
+
+  const [error, setError] = useState({   //=> validation state (only for username field)
+    username: "",  
+  });
+
+  const onChange = (e) => {
+    const { value, name, type, checked } = e.target;
+    setForm((state) => ({
+      ...state,
+      [name]: type === "checkbox" ? checked : value,
+    }));
+  };
+
+  const submitHandler = (e) => {
+    e.preventDefault();
+    if(form.username.length < 3){
+      // error
+    setError((state) => ({
+        ...state,
+        name: "Needs More Than 2 Characters",
+      }))
+    return;
+
+    } else {
+      setForm({
+        username: "",
+        email: "",
+        comment: "",
+        country: "", //=> no default value after submitting
+        gender: "", //=> no default value submitting
+        agree: false,
+      })
+      setError((state) => ({
+        ...state,
+        name: "",
+      }))
+    }
+      console.log(form)
+  };
+
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <form
+      className="shadow-lg p-3 col-9 mt-5 m-auto" onSubmit={(e) => submitHandler(e)}>
+      <label>
+        Username:
+        <input value={form.username} onChange={onChange} name="username" />
+      </label>
+      {error.name && <div className="text-danger">{error.name}</div>}
+      <hr />
+      <label>
+        Email:
+        <input value={form.email} onChange={onChange} name="email" />
+      </label>
+      <hr />
+      <label>
+        Comment:
+        <textarea value={form.comment} onChange={onChange} name="comment" />
+      </label>
+      <hr />
+      <label>
+        Country:
+        <select onChange={onChange} name="country" value={form.country}>
+          <option value="usa">USA</option>
+          <option value="uk">UK</option>
+          <option value="canada">Canada</option>
+        </select>
+      </label>
+      <hr />
+      <label>
+        Gender
+        <div>
+      <input type="radio" name="gender" onChange={onChange} value="male" checked={form.gender === "male"} /> Male
+      <input type="radio" name="gender" onChange={onChange} value="female" checked={form.gender === "female"}  /> Female
+      <input type="radio" name="gender" onChange={onChange} value="other" checked={form.gender === "other"}  /> Other
+        </div>
+      </label>
+      <hr />
+      <label>
+        Agree
+        <input type="checkbox" onChange={onChange} name="agree" value={form.agree} />
+      </label>
+      <hr />
+      <button>Submit</button>
+    </form>
   );
 }
 
